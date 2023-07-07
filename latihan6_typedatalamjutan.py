2dc664b02070bfbff260284b099e062e3a76b745
Anggunlord <136144932+Anggunlord@users.noreply.github.com>
2023-07-07 16:39:36 +0700

    Update latihan6_typedatalamjutan.py
    anggun cool

--- latihan6_typedatalamjutan.py
+++ latihan6_typedatalamjutan.py
@@ -1,1 +1,26 @@
-maaf belom di isi tunggu acara selanjutnya
+#cetak identitas
+nim = 20122056
+nama = "Anggun Widyaningsih"
+kelas = "TI A"
+prodi = "Informatika"
+
+#Cetak data lingkaran
+
+print("-----------------------------------------------------------")
+print("NIM Anda                  : %d" % (nim))
+print("Nama Lengkap Anda         :%s" % (nama))
+print("Kelas                     :%s" % (kelas))
+print("Program Studi             :%s" % (prodi))
+print("-----------------------------------------------------------")
+print("Cetak Semua Data Lingkaran")
+nama = input("Masukan Nama Bangunan  :")
+jari_jari = input("Masukan r  :")
+phi = input("Masukan phi  :")
+print("-----------------------------------------------------------")
+print("Nama bangunan : %s" % (nama))
+print("Jari_Jari : %s" % (jari_jari))
+print("Phi : %s" % (phi))
+print("-----------------------------------------------------------")
+#aritmatika
+print("Mahasiswa Uniss Batang pada semester 2 diruang lab belajar dengan topik tipe data dan algoritma pemrograman")
+
